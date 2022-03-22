4ee978799f513d24c19c4384460dc2fafe612c4a
jveldmaat <j.veldmaat@student.han.nl>
2022-03-22 10:40:26 +0100

    Main edited

--- src/main/java/com/github/neotje/DepthsDescending/DepthsDescending.java
+++ src/main/java/com/github/neotje/DepthsDescending/DepthsDescending.java
@@ -1,17 +1,26 @@
 package com.github.neotje.DepthsDescending;
 
+import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.YaegerGame;
+import com.github.neotje.DepthsDescending.Scenes.TitleScene;
 
 public class DepthsDescending extends YaegerGame {
+
+    public static void main(String[] args) {
+        launch(args); }
+
     @Override
     public void setupGame() {
+        setGameTitle("Descending Depths");
+        setSize(new Size(800, 400));
 
     }
 
     @Override
     public void setupScenes() {
+    addScene(0, new TitleScene(this));
 
     }
 
-    public static void main(String[] args) { launch(args); }
+
 }
---
+++ src/main/java/com/github/neotje/DepthsDescending/Sprites/Buttons/Button.java
@@ -0,0 +1,4 @@
+package com.github.neotje.DepthsDescending.Sprites.Buttons;
+
+public class Button {
+}
