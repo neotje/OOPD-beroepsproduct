5de108da93f73a48e531f204ed77117c63c00d9b
Neo Hop <neotje111@gmail.com>
2022-03-22 10:24:10 +0100

    Alle scenes naar Scene mapje

--- src/main/java/com/github/neotje/DepthsDescending/Scenes/GameScene.java
+++ src/main/java/com/github/neotje/DepthsDescending/Scenes/GameScene.java
@@ -1,4 +1,15 @@
 package com.github.neotje.DepthsDescending.Scenes;
 
-public class GameScene {
+import com.github.hanyaeger.api.scenes.DynamicScene;
+
+public class GameScene extends DynamicScene {
+    @Override
+    public void setupScene() {
+
+    }
+
+    @Override
+    public void setupEntities() {
+
+    }
 }
