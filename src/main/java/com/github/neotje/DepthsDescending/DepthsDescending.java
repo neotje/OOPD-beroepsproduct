3186617a2260ed0b8725251aa3e109c84bbca393
jveldmaat <j.veldmaat@student.han.nl>
2022-03-23 09:34:53 +0100

    Commit for Brian

--- src/main/java/com/github/neotje/DepthsDescending/DepthsDescending.java
+++ src/main/java/com/github/neotje/DepthsDescending/DepthsDescending.java
@@ -5,8 +5,10 @@ import com.github.hanyaeger.api.YaegerGame;
 import com.github.neotje.DepthsDescending.Rooms.Room;
 import com.github.neotje.DepthsDescending.Scenes.GameScene;
 import com.github.neotje.DepthsDescending.Scenes.TitleScene;
+import com.github.neotje.DepthsDescending.Entities.Player;
 
 public class DepthsDescending extends YaegerGame {
+    //Player player1 = new Player();
 
     public static void main(String[] args) {
         launch(args); }
@@ -21,7 +23,7 @@ public class DepthsDescending extends YaegerGame {
     @Override
     public void setupScenes() {
     addScene(0, new TitleScene(this));
-    addScene(1, new Room("background/room.png"));
+    addScene(1, new Room("background/room10.png"));
 
     }
 
