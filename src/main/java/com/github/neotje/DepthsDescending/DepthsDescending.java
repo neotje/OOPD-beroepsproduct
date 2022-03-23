256faa2b2ce99a6542b2619685c1404b76745f79
jveldmaat <j.veldmaat@student.han.nl>
2022-03-23 10:17:06 +0100

    added multiple Scenes

--- src/main/java/com/github/neotje/DepthsDescending/DepthsDescending.java
+++ src/main/java/com/github/neotje/DepthsDescending/DepthsDescending.java
@@ -4,6 +4,7 @@ import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.YaegerGame;
 import com.github.neotje.DepthsDescending.Rooms.Room;
+import com.github.neotje.DepthsDescending.Scenes.EndScene;
 import com.github.neotje.DepthsDescending.Scenes.GameScene;
 import com.github.neotje.DepthsDescending.Scenes.TitleScene;
 import com.github.neotje.DepthsDescending.Entities.Player;
@@ -25,6 +26,9 @@ public class DepthsDescending extends YaegerGame {
     public void setupScenes() {
     addScene(0, new TitleScene(this));
     addScene(1, new Room("background/room.png"));
+    addScene(2, new Room("background/room9.png"));
+    addScene(3, new Room("background/room10.png"));
+    addScene(4, new EndScene());
 
     }
 
--- src/main/java/com/github/neotje/DepthsDescending/Scenes/EndScene.java
+++ src/main/java/com/github/neotje/DepthsDescending/Scenes/EndScene.java
@@ -1,6 +1,11 @@
 package com.github.neotje.DepthsDescending.Scenes;
 
+import com.github.hanyaeger.api.AnchorPoint;
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.entities.impl.TextEntity;
 import com.github.hanyaeger.api.scenes.StaticScene;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class EndScene extends StaticScene {
     @Override
@@ -10,6 +15,11 @@ public class EndScene extends StaticScene {
 
     @Override
     public void setupEntities() {
+        var titleText = new TextEntity(new Coordinate2D(getWidth()/2, getHeight()/2), "Endscreen");
 
+        titleText.setAnchorPoint(AnchorPoint.CENTER_CENTER);
+        titleText.setFont(Font.font("fonts/RINGM___.tiff", FontWeight.SEMI_BOLD, 80));
+
+        addEntity(titleText);
     }
 }
